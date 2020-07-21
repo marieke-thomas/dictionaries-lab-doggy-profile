72900ebfafad649a7ba0511491eaeceb502d4270
marieke-thomas <marieke.thomas@gmail.com>
2020-07-21 22:27:10 +0000

    Updated the dictionary file

--- 2_tofu_dictionary.py
+++ 2_tofu_dictionary.py
@@ -11,14 +11,45 @@ cutedoggy32 = {
 }
 
 # Read a dictionary
-print(cutedoggy32["age"])
+# print(cutedoggy32["age"])
 
 # Update a dictionary
-cutedoggy32["age"] = 8
-print(cutedoggy32)
+# cutedoggy32["age"] = 8
+# print(cutedoggy32)
 # Happy birthday Tofu
 
 
 # Delete
-cutedoggy32.pop("favorite activity")
+# cutedoggy32.pop("favorite activity")
+# print(cutedoggy32)
+
+# Challenges! The last line prints the dictionary so you can check if your updates are working!
+#1 Print out Tofu's fur color
+
+
+#2 Tofu got bored of the squeaky carrot. Change his favorite toy to "tennis ball"
+
+
+#3 Tofu's profile is incomplete! Come up with another category and response, and add them to the dictionary
+
+
+#4 Delete Tofu's favorite activity from the dictionary
+
+
+#5 For each category in the profile, print out a statement that says "Tofu's ____ is ____" and fill in the blanks.
+
+
+# 6 Tofu's middle name is "Seitan". Add that to the profile
+
+
+# 7 Print Tofu's middle name
+
+
+#8 A lot of sites include a section for what someone is looking for. Create a list of 3 adjectives that Tofu is looking for in a doggie friend and add the list to the dictionary (you will have a list within a dictionary)
+
+
+
+# 9 Print out the second of the adjectives that you just came up with
+
+
 print(cutedoggy32)
