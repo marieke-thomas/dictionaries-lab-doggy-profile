faa8a247868bc3c5e4d4525e9791b7cca9d642bb
marieke-thomas <32941729+marieke-thomas@users.noreply.github.com>
2024-07-09 14:59:17 -0400

    Update 1_tofu_profile.py

--- 1_tofu_profile.py
+++ 1_tofu_profile.py
@@ -1,6 +1,5 @@
 cutedoggy32 = ["Tofu", "Thomas", 7, "brown", "terrier", "squeaky carrot", 19, "long walks in the park"]
 
 # print out Tofu's favorite toy
-print(cutedoggy32[5])
 
-# Lists are most useful when you have a lot of items in the same category
+# print out Tofu's age
